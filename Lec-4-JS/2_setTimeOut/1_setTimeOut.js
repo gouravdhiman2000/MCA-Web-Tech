243e921d08a40ca2b0bd32f8145e29e84e73438f
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2025-10-30 12:31:17 +0530

    Learn about basic setTimeOut Functionality

---
+++ Lec-4-JS/2_setTimeOut/1_setTimeOut.js
@@ -0,0 +1,14 @@
+//Syntax->
+// setTimeout(function(){
+
+// }, milliseconds)
+
+setTimeout(function(){
+    console.log("Hello World!")
+}, 5000);
+// 5 seconds ka dlay browser par dal diya jab 5 seconds poore j=honge tab function ko call kr dega
+
+console.log("Hi !");
+console.log("kya hal chal hai");
+console.log("aur kya ho rha hai?");
+console.log("free hai chle kahi?");
---
+++ Lec-4-JS/2_setTimeOut/example.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+    console.log("Hello World!")
+}, 5000);
+
+setTimeout(function(){
+    console.log("Aur Veere")
+}, 4000);
+
+for ( let i = 0;i < 10000 ; i++){
+    console.log("kya hal chal hai");
+}
+
+// console.log("Hi!");
+// console.log("kya hal chal hai");
+// console.log("aur kya ho rha hai?");
+// console.log("free hai chle kahi?");
