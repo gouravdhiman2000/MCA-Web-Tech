781e10f5c39f01e44556750de30b89ce8e99bcb3
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2025-10-30 13:10:47 +0530

    learn advance concept of selecting a html elements

---
+++ Lec-8-JS/2_selectingElementAdvanced/script.js
@@ -0,0 +1,19 @@
+// 1. using querySelector : will give only on element
+//  using querySelectorAll : gives collection of result
+let movies = document.querySelectorAll('.movie');
+console.log(movies);
+
+// 2. Selecting id using querySelector
+
+let gadar = document.querySelector('#gadar');
+console.log(gadar);
+
+// 3. Tagname se lekar aaana hai
+let div= document.querySelectorAll('div');
+console.log(div);
+
+// 4. Pick the li item that has the class name - ironman
+let ironman = document.querySelector('li.ironman');
+console.log(ironman);
+
+
