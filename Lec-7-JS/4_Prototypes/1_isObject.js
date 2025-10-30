d031ec6059b921cac861009bd964881a90483d6a
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2025-10-30 13:08:27 +0530

    Learn about Prototypes and its internal working

---
+++ Lec-7-JS/4_Prototypes/1_isObject.js
@@ -0,0 +1,8 @@
+function Person(){
+    console.log("Hello");
+}
+Person();
+// person is a object whose key is name and value is Gourav
+Person.myName = "Gourav";
+console.log(Person.myName);
+// person is an function and here it can be used as an object
