c9711b01056dbbca3c999a0e5f8a3e67a51538b2
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2025-10-30 13:09:50 +0530

    learn different ways of selecting the html elements

---
+++ Lec-8-JS/1_selectingElements/script.js
@@ -0,0 +1,21 @@
+
+// console.log("Hello World!");
+
+// Selecting Elements by Tagname
+
+let divs = document.getElementsByTagName('div');
+console.log(divs);
+
+// Selecting Elements by Classname
+
+let movie = document.getElementsByClassName('movie');
+console.log(movie);
+
+// Selecting Elements by ID
+
+const avengers = document.getElementById('avengers');
+console.log(avengers);
+
+
+let movieList = document.getElementById("movielist");
+console.log(movieList);
