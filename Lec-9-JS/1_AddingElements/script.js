fbc5aad01448b6f26eeabb569f07a65f5dbfa732
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2025-11-01 10:57:31 +0530

    Updated improved version of adding element

--- Lec-9-JS/1_AddingElements/script.js
+++ Lec-9-JS/1_AddingElements/script.js
@@ -16,6 +16,17 @@ let movies = ["Avengers", "IronMan", "Hulk", "Thor", "SpiderMen", "Gadar"];
 //     i++;
 // })
 
+let imgLinks = [
+    'https://media.tenor.com/MC-iCY27otwAAAA1/khatam-tata-bye-bye.webp',
+
+    'https://media.tenor.com/2O8B2MXy4i4AAAA1/rahul-gandhi-vote-chor.webp',
+
+    'https://media.tenor.com/JQRUO7hMqVIAAAA1/galti-kar.webp',
+
+    'https://media.tenor.com/ATCFTrWThTsAAAA1/rahul-gandhi-narendra-modi.webp',
+
+    'https://media.tenor.com/rtMWR6GaCGQAAAA1/hi-rahul-gandhi.webp'
+]
 
 let i=0;
 
@@ -23,7 +34,9 @@ btn.addEventListener('click', ()=>{
     console.log(movieList.innerHTML);
 
     if(i==movies.length){
-        let url = 'https://media.tenor.com/MC-iCY27otwAAAA1/khatam-tata-bye-bye.webp';
+        // let url = 'https://media.tenor.com/MC-iCY27otwAAAA1/khatam-tata-bye-bye.webp';
+
+        let url = imgLinks[Math.floor((Math.random() * imgLinks.length))];
 
         // movieList.innerHTML += `<li><img src=${url}></li>`;
         // i++;
